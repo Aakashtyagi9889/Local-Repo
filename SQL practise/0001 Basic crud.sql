71aa556dbac99a251e5919bc2995e0ba5e599d78
Aakashtyagi9889 <aakashtyagi9889@gmail.com>
2026-08-04 09:19:01 +0530

    SQL Day 1

---
+++ SQL practise/0001 Basic crud.sql
@@ -0,0 +1,15 @@
+create table students(
+student_id int,
+name varchar(100),
+age int,
+city varchar(50)
+);
+
+insert into students values
+(1, 'Aakash' , 23 , 'Ghaziabad'),
+(2, 'Harsh' , 23 , 'Ghaziabad'),
+(3, 'Lavi' , 27 , 'Hapu'),
+(4, 'Deepak' , 27 , 'Hapur'),
+(5, 'Sandeep' , 47 , 'Delhi');
+
+select * from students;
